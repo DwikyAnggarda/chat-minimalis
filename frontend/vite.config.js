571cd1fe9e509b579a75425aa81562115aaa5be6
Dwiky Anggarda <dwiky.amin@gmail.com>
2025-11-20 10:17:04 +0700

    Update allowedHosts in Vite configuration for local development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['chatmin-frontend.loca.lt', 'chatmin-backend.loca.lt', '17e3bc76a114.ngrok-free.app', 'https://8026af3bddba.ngrok-free.app', 'http://127.0.0.1:5173', 'http://127.0.0.1:8000'],
+    allowedHosts: ['chatmin-frontend.loca.lt', 'chatmin-backend.loca.lt', '17e3bc76a114.ngrok-free.app', '8026af3bddba.ngrok-free.app', '127.0.0.1:5173', '127.0.0.1:8000', 'demo.hexaintegra.com'],
   },
 })
